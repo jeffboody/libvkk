jeffboody/libvkk

commit 3bd82bfced39a0d24a84f1059248afc88fa9c631

add offset to vkk_compute_writeBuffer

core/vkk_engine.c
@@ -1491,7 +1491,7 @@ vkk_engine_t* vkk_engine_new(vkk_platform_t* platform,
 
 	self->version.major = 1;
 	self->version.minor = 1;
-	self->version.patch = 53;
+	self->version.patch = 54;
 
 	// app info
 	snprintf(self->app_name, 256, "%s", app_name);

core/vkk_memoryManager.c
@@ -425,7 +425,7 @@ vkk_memoryManager_allocBuffer(vkk_memoryManager_t* self,
 
 	if(buf)
 	{
-		vkk_memoryManager_write(self, memory, size, buf);
+		vkk_memoryManager_write(self, memory, size, 0, buf);
 	}
 
 	vkk_memoryManager_chunkLock(self, memory->chunk);
@@ -566,11 +566,11 @@ void vkk_memoryManager_free(vkk_memoryManager_t* self,
 void vkk_memoryManager_write(vkk_memoryManager_t* self,
                              vkk_memory_t* memory,
                              size_t size,
+                             size_t offset,
                              const void* buf)
 {
 	ASSERT(self);
 	ASSERT(memory);
-	ASSERT(size > 0);
 	ASSERT(buf);
 
 	vkk_memoryChunk_t*   chunk  = memory->chunk;
@@ -578,19 +578,19 @@ void vkk_memoryManager_write(vkk_memoryManager_t* self,
 	vkk_memoryManager_t* mm     = pool->mm;
 	vkk_engine_t*        engine = mm->engine;
 
-	vkk_memoryManager_chunkLock(self, chunk);
-
-	if(size > pool->stride)
+	if((size == 0) || (size + offset > pool->stride))
 	{
-		LOGE("invalid size=%" PRIu64 ", stride=%" PRIu64,
-		     (uint64_t) size, (uint64_t) pool->stride);
+		LOGE("invalid size=%" PRIu64 ", offset=%" PRIu64
+		     ", stride=%" PRIu64,
+		     (uint64_t) size, (uint64_t) offset,
+		     (uint64_t) pool->stride);
 		vkk_memoryManager_chunkUnlock(self, chunk);
 		return;
 	}
 
 	void* data;
 	if(vkMapMemory(engine->device, chunk->memory,
-	               memory->offset, size, 0,
+	               memory->offset + offset, size, 0,
 	               &data) == VK_SUCCESS)
 	{
 		memcpy(data, buf, size);

core/vkk_compute.c
@@ -292,14 +292,14 @@ vkk_compute_updateMode(vkk_compute_t* self)
 }
 
 void
-vkk_compute_updateBuffer(vkk_compute_t* self,
-                         vkk_buffer_t* buffer,
-                         size_t size,
-                         const void* buf)
+vkk_compute_writeBuffer(vkk_compute_t* self,
+                        vkk_buffer_t* buffer,
+                        size_t size,
+                        size_t offset,
+                        const void* buf)
 {
 	ASSERT(self);
 	ASSERT(buffer);
-	ASSERT((size > 0) && (size <= buffer->size));
 	ASSERT(buf);
 	ASSERT(vkk_compute_checkUpdateType(buffer));
 
@@ -307,7 +307,7 @@ vkk_compute_updateBuffer(vkk_compute_t* self,
 
 	uint32_t idx = 0;
 	vkk_memoryManager_write(engine->mm, buffer->memory[idx],
-	                        size, buf);
+	                        size, offset, buf);
 }
 
 void

core/vkk_imageUploader.c
@@ -570,7 +570,7 @@ int vkk_imageUploader_upload(vkk_imageUploader_t* self,
 		ub = (vkk_uploaderBuffer_t*)
 		     cc_multimap_remove(self->buffer_map, &miter);
 		vkk_memoryManager_write(engine->mm, ub->memory,
-		                        size, pixels);
+		                        size, 0, pixels);
 	}
 	else
 	{

core/vkk_memoryManager.h
@@ -66,6 +66,7 @@ void                 vkk_memoryManager_free(vkk_memoryManager_t* self,
 void                 vkk_memoryManager_write(vkk_memoryManager_t* self,
                                              vkk_memory_t* memory,
                                              size_t size,
+                                             size_t offset,
                                              const void* buf);
 void                 vkk_memoryManager_read(vkk_memoryManager_t* self,
                                             vkk_memory_t* memory,

core/vkk_renderer.c
@@ -848,7 +848,7 @@ void vkk_renderer_updateBuffer(vkk_renderer_t* self,
 	}
 
 	vkk_memoryManager_write(engine->mm, buffer->memory[idx],
-	                        size, buf);
+	                        size, 0, buf);
 }
 
 void

core/xsq-test/xsq_test.c
@@ -233,12 +233,12 @@ void xsq_test_main(xsq_test_t* self,
 		xx1[i] = x[i]*x[i];
 	}
 
-	vkk_compute_updateBuffer(self->compute, self->sb00_x,
-	                         size, x);
+	vkk_compute_writeBuffer(self->compute, self->sb00_x,
+	                        size, 0, x);
 
 	uint32_t count = XSQ_TEST_COUNT;
-	vkk_compute_updateBuffer(self->compute, self->sb02_count,
-	                         sizeof(uint32_t), &count);
+	vkk_compute_writeBuffer(self->compute, self->sb02_count,
+	                        sizeof(uint32_t), 0, &count);
 
 	// compute xsq
 	vkk_compute_bindComputePipeline(self->compute, self->cp);

core/xsum-test/xsum_test.c
@@ -233,12 +233,12 @@ void xsum_test_main(xsum_test_t* self,
 		xsum1 += x[i];
 	}
 
-	vkk_compute_updateBuffer(self->compute, self->sb00_x,
-	                         size, x);
+	vkk_compute_writeBuffer(self->compute, self->sb00_x,
+	                        size, 0, x);
 
 	uint32_t count = XSUM_TEST_COUNT;
-	vkk_compute_updateBuffer(self->compute, self->sb02_count,
-	                         sizeof(uint32_t), &count);
+	vkk_compute_writeBuffer(self->compute, self->sb02_count,
+	                        sizeof(uint32_t), 0, &count);
 
 	// compute xsum
 	// only dispatch one workgroup to compute the xsum since

commit 41b1546c9b808c8948b5b00a717972252e08f597

fix locking issue
locking issue introduced by:

3bd82bf add offset to vkk_compute_writeBuffer

core/vkk_engine.c
@@ -1491,7 +1491,7 @@ vkk_engine_t* vkk_engine_new(vkk_platform_t* platform,
 
 	self->version.major = 1;
 	self->version.minor = 1;
-	self->version.patch = 55;
+	self->version.patch = 56;
 
 	// app info
 	snprintf(self->app_name, 256, "%s", app_name);

core/vkk_memoryManager.c
@@ -578,6 +578,8 @@ void vkk_memoryManager_write(vkk_memoryManager_t* self,
 	vkk_memoryManager_t* mm     = pool->mm;
 	vkk_engine_t*        engine = mm->engine;
 
+	vkk_memoryManager_chunkLock(self, chunk);
+
 	if((size == 0) || (size + offset > pool->stride))
 	{
 		LOGE("invalid size=%" PRIu64 ", offset=%" PRIu64